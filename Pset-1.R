9988181f200e71badca6b80672e7973f0d7c5ac0
AngelinaShi1 <ashi1031@uchicago.edu>
2022-04-08 09:46:00 -0500

    update question number

--- Pset-1.R
+++ Pset-1.R
@@ -1,16 +1,18 @@
-library(ggplot2)
+#8a
 n <- 10000
 mu <- 0
 sigma <- 1
 x <- rnorm(n, mu, sigma)
 ggplot(data.frame(x))+aes(x)+geom_histogram(bins=100)
 
+#8b
 n <- 10000
 min_y <- -1
 max_y <- 1
 y <- runif(n, min_y, max_y)
 ggplot(data.frame(y))+aes(y)+geom_histogram(bins=100)
 
+#9b
 my_rbernoulli <- function(n, p) { 
   U <- runif(n, min=0, max=1)# [INSERT YOUR CODE HERE] 
   x <- U <= p# Return draws
