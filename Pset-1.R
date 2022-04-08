ab7d5c8152509b25b9d0120e8aa7d6feaea8c7bd
AngelinaShi1 <ashi1031@uchicago.edu>
2022-04-08 09:45:17 -0500

    update pset 9

--- Pset-1.R
+++ Pset-1.R
@@ -10,3 +10,13 @@ min_y <- -1
 max_y <- 1
 y <- runif(n, min_y, max_y)
 ggplot(data.frame(y))+aes(y)+geom_histogram(bins=100)
+
+my_rbernoulli <- function(n, p) { 
+  U <- runif(n, min=0, max=1)# [INSERT YOUR CODE HERE] 
+  x <- U <= p# Return draws
+  return(x)
+}#MY_RBERNOULLI
+# Test the custom Bernoulli generator function
+x <- my_rbernoulli(10000, 0.5)
+length(x) == 10000 # should return TRUE
+mean(x) # should a number near 0.5
