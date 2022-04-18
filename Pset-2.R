a1c4630bf3a09f92196c869c3f965116f6961950
AngelinaShi1 <ashi1031@uchicago.edu>
2022-04-18 15:26:08 -0500

    update d & e

--- Pset-2.R
+++ Pset-2.R
@@ -3,12 +3,16 @@ yrs_educ <- df$YRS_EDUC
 wkly_wage <- df$WKLY_WAGE
 has_college_degree <- yrs_educ == 16
 mean(has_college_degree)
-mu_college <- mean((has_college_degree)*(wkly_wage))/mean(has_college_degree)
+mu_college <- sum((has_college_degree)*(wkly_wage)) / sum(has_college_degree)
 mu_college
 n = 329309
-se_college <- 1/(sqrt(n))*sqrt((mu_college^2)/mean(has_college_degree))
+x <- sum(has_college_degree * wkly_wage^2) / sum(has_college_degree) 
+se_college <- (1/(sqrt(n))*sqrt(x-mu_college^2))/sqrt(mean(has_college_degree))
 se_college
 cn1 <- mu_college-1.96*se_college
 cn1
 cn2 <- mu_college+1.96*se_college
 cn2
+t.test()
+
+
