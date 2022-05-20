f2b7acd4816abce600a8247c726791c19f3c2954
AngelinaShi1 <ashi1031@uchicago.edu>
2022-05-20 07:45:07 -0500

    update ab

---
+++ Pset-4.R
@@ -0,0 +1,18 @@
+dat <- read.csv("data/bw06.csv")
+dat <- as.matrix(dat)
+n <- 9800
+y <- dat[, "birthweight"]
+w <- dat[, "cigsdaily"]
+x <- cbind(1, dat[, c("boy", "age", "highschool", "somecollege", "college")])
+x_tld <- dat[, "married"]
+
+#a
+beta=(mean(y*w)-mean(y)*mean(w))/(mean(w^2)-(mean(w)^2))
+beta
+alpha=mean(y)-mean(w)*beta
+alpha
+
+#b
+beta_w <- solve(w %*% t(w))*(w %*% y)
+beta_w
+
